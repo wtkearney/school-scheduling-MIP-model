8b756c986a90ae0074128193d2891a1a777cc4f4
Will Kearney <wtkearney@gmail.com>
2016-11-22 13:42:02 -0800

    Added new constraints

--- scheduling_model.py
+++ scheduling_model.py
@@ -148,7 +148,7 @@ def build_model():
 					quicksum(X[student, course, period] for period in periods) == 1,
 					name='same_core_class_assignment_{}_{}'.format(student, course))
 
-	print("Assign students and teachers to lunch")
+	print("Assign students and teachers to lunch in either period {} or {}".format(lunch_periods[0], lunch_periods[1]))
 	for student in students:
 		model.addConstr(
 			X[student, "Lunch1", lunch_periods[0]]
@@ -157,21 +157,30 @@ def build_model():
 			+ X[student, "Lunch2", lunch_periods[1]] == 1,
 			name="assign_student_lunch_{}".format(student))
 
-	print("Ensure each student takes PE once a day")
-	for student in students:
-		if grade[student] == 6:
-			PE_courseName = "PE6"
-		elif grade[student] == 7:
-			PE_courseName = "PE7"
-		elif grade[student] == 8:
-			PE_courseName = "PE8"
-		elif grade[student] == 5:
-			continue
-		else:
-			print grade[student]
-		model.addConstr(
-			quicksum(X[student, PE_courseName, period] for period in periods) == 1,
-			name='PE_requirement_{}'.format(student))
+	for teacher in staff_list:
+		# only one teacher is currently teaching lunch; thus the model is infeasible if this if statement
+		# is removed because they're the only one who _can_ teach lunch, and thus have to teach both sections
+		if staff_course[teacher, "Lunch1"] != 1 and staff_course[teacher, "Lunch2"] != 1:
+			model.addConstr(
+				quicksum(Y[teacher, course, lunch_periods[0]] +
+					Y[teacher, course, lunch_periods[1]] for course in courses) <= 1,
+					name="assign_teacher_lunch_break_{}".format(teacher))
+
+	# print("Ensure each student takes PE once a day")
+	# for student in students:
+	# 	if grade[student] == 6:
+	# 		PE_courseName = "PE6"
+	# 	elif grade[student] == 7:
+	# 		PE_courseName = "PE7"
+	# 	elif grade[student] == 8:
+	# 		PE_courseName = "PE8"
+	# 	elif grade[student] == 5:
+	# 		continue
+	# 	else:
+	# 		print grade[student]
+	# 	model.addConstr(
+	# 		quicksum(X[student, PE_courseName, period] for period in periods) == 1,
+	# 		name='PE_requirement_{}'.format(student))
 
 	numCurrentElectives = {}
 	for student in students:
@@ -181,10 +190,18 @@ def build_model():
 			quicksum(X[student, course, period] * (1-core[course]) * student_course[student,course] for course in courses for period in periods),
 			name='link_num_current_electives_{}'.format(student))
 
+	print("Creating class size minimax variables")
+	maxClassSize = {}
+	for course in courses:
+		maxClassSize[course] = model.addVar(lb=0,ub=max_class_size[course],vtype=GRB.INTEGER,
+			name='maxClassSize_{}'.format(course))
+	
+	for course in courses:
+		for period in periods:
+			model.addConstr(
+				maxClassSize[course] >= quicksum(X[student, course, period] for student in students),
+				name='minimax_classSize_link_{}_{}'.format(course, period))
 
-	model.setObjective(quicksum(numCurrentElectives[student] for student in students))
-
-	model.ModelSense = GRB.MAXIMIZE
 
 	# print("Ensure student can't take a given class type more than twice per day")
 	# for student in students:
@@ -201,8 +218,29 @@ def build_model():
 	# 				quicksum(X[student, course, period] for period in periods) == 1,
 	# 				name='force_class_assignment_{}_{}'.format(student, course))
 
+	print("Building objective function")
+	objectiveList = []
+
+	totalElectiveCountWeight = -1
+	totalElectiveCount = model.addVar(vtype=GRB.INTEGER, name='totalElectiveCount')
+	model.addConstr(totalElectiveCount == quicksum(numCurrentElectives[student] for student in students),
+		name='linkTotalElectiveCount')
+	objectiveList.append((totalElectiveCountWeight, totalElectiveCount))
+
+	# totalMinimaxClassSizeWeight = 1
+	# totalMinimaxClassSize = model.addVar(vtype=GRB.INTEGER, name='totalMinimaxClassSize')
+	# model.addConstr(totalMinimaxClassSize == quicksum(maxClassSize[course] for course in courses),
+	# 	name='linkTotalMinimaxClassSize')
+	# objectiveList.append((totalMinimaxClassSizeWeight, totalMinimaxClassSize))
+
+	# add the objective variables to the gurobi model
+	model.setObjective(quicksum(weight*var for (weight,var) in objectiveList))
+	model.ModelSense = GRB.MINIMIZE
+
 	model.update()
 
+	print "\nObjective:"
+	print model.getObjective()
 
 	return model
 
